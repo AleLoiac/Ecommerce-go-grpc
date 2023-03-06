2d30e3b70bb0eca15980b0ca16e26e79d7c20417
AleLoiac <alessandroloiac@gmail.com>
2023-03-06 14:56:46 +0100

    06/03

--- server/eco_server.go
+++ server/eco_server.go
@@ -2,13 +2,50 @@ package main
 
 import (
 	"Ecommerce/user/userpb"
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/dgraph-io/badger/v3"
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 )
+
+func (s *server) CreateUser(ctx context.Context, req *userpb.CreateUserRequest) (*userpb.CreateUserResponse, error) {
+
+	fmt.Printf("CreateUser function is invoked with %v\n", req)
+
+	user := &userpb.User{
+		Id:       uuid.New().String(),
+		Username: req.Username,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	err := db.Update(func(txn *badger.Txn) error {
+		userBytes, err := json.Marshal(user)
+		if err != nil {
+			return err
+		}
+
+		return txn.Set([]byte(user.Id), userBytes)
+	})
+
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Failed to create user: %v", err),
+		)
+	}
+
+	return &userpb.CreateUserResponse{
+		UserId: user.Id,
+	}, nil
+}
 
 type server struct {
 	userpb.UserServiceServer
@@ -19,7 +56,7 @@ var db *badger.DB
 func main() {
 	fmt.Println("Server started...")
 
-	db, _ = badger.Open(badger.DefaultOptions("/Users/aless/Desktop/Go/grpc_ToDo_badger/db"))
+	db, _ = badger.Open(badger.DefaultOptions("/Users/aless/Desktop/Go/Ecommerce/db"))
 
 	defer db.Close()
 
